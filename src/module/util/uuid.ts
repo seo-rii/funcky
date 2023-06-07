6e5a6856c966a9fea4ee2217fc8dcc934e9a6049
seo-rii <me@seorii.page>
2023-06-07 14:45:31 +0900

    feat: add uuid util function

--- src/module/util/uuid.ts
+++ src/module/util/uuid.ts
@@ -4,9 +4,10 @@ import short from 'short-uuid';
 const base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^`{|}~";
 const tr = short(base);
 
-type Uuid = typeof uuidv4 & { encode: (string) => string, decode: (string) => string };
+type Uuid = typeof uuidv4 & { short: () => string, encode: (string) => string, decode: (string) => string };
 
 const uuid: Uuid = <Uuid>uuidv4;
+uuid.short = tr.generate;
 uuid.encode = tr.fromUUID;
 uuid.decode = tr.toUUID;
 
