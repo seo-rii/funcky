80cfb340e78130e18f0d98516e180fc3c5775397
Seohyun Lee <studioseorii@gmail.com>
2023-06-12 13:49:42 +0900

    fix: fix uuid shorter

--- src/module/util/uuid.ts
+++ src/module/util/uuid.ts
@@ -1,14 +1,22 @@
 import {v4 as uuidv4} from 'uuid';
 import short from 'short-uuid';
 
-const base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%'()*+-.:<=>@[]^`{|}~";
-const tr = short(base);
+const min = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-.:<=>?@[]^`{|}~/";
+const sh = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_.";
+const trm = short(min);
+const trs = short(sh);
 
-type Uuid = typeof uuidv4 & { short: () => string, encode: (string) => string, decode: (string) => string };
+type Uuid = typeof uuidv4 & {
+    short: () => string, encode: (string) => string, decode: (string) => string,
+    mshort: () => string, mencode: (string) => string, mdecode: (string) => string,
+};
 
 const uuid: Uuid = <Uuid>uuidv4;
-uuid.short = tr.generate;
-uuid.encode = tr.fromUUID;
-uuid.decode = tr.toUUID;
+uuid.short = trs.generate;
+uuid.encode = trs.fromUUID;
+uuid.decode = trs.toUUID;
+uuid.mshort = trm.generate;
+uuid.mencode = trm.fromUUID;
+uuid.mdecode = trm.toUUID;
 
 export default uuid;
